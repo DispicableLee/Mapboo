e35bb4436f3b098eb2505e13772b76d1297d8d50
Colleen Cabedo <cmcabedo@gmail.com>
2022-10-25 11:01:08 -0400

    added components

---
+++ mapboo/src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Header(){
+
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        Map
+                    </li>
+                    <li>
+                        Form
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
