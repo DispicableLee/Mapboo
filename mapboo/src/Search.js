ad94bbeda2b83ce852333718a9da0f46c32da0dc
DispicableLee <lee.robert053@gmail.com>
2022-10-25 10:57:36 -0400

    done

--- mapboo/src/Search.js
+++ mapboo/src/Search.js
@@ -10,14 +10,27 @@ export default function Search() {
 
     function searchLocation(e){
         e.preventDefault()
-        let test = placeName.split(" ")
-        let testing = test[0]
-        for(let i = 1; i<=test.length-1; i++){
-            testing = testing + "%20" + test[i] 
+        //=============sort by place ==================
+        let testPlace = placeName.split(" ")
+        let place = testPlace[0]
+        for(let i = 1; i<=testPlace.length-1; i++){
+            place = place + "%20" + testPlace[i] 
         }
-        console.log(testing)
+        console.log(place)
+        //==========  sort by city =================
+        let testCity = city.split(" ")
+        let cityName = ""
+        for (let i = 0; i<testCity.length; i++){
+            cityName = testCity[0] + "%20" + testCity[i]
+        }
+        console.log(cityName)
+        //========== sort by state ================
+        let testState = state.split(" ")
+        let stateName = testState
     }
 
+const testUrl = "https://api.geoapify.com/v1/geocode/search?name={place}%20&postcode={postCode}&city={cityname}&country%20United%20States%of%20America&format=json&apiKey=f67baa6095514751976876bc73fb1f06"
+    
 
   var requestOptions = {
     method: "GET",
@@ -28,7 +41,7 @@ export default function Search() {
     requestOptions
   )
     .then((response) => response.json())
-    .then(console.log)
+    .then(json=>console.log(json.results[0]))
     .catch((error) => console.log("error", error));
   return (
     <div>
