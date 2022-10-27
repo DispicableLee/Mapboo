82046dde1bb3f84563dc2e897685515c0fc664bb
Colleen Cabedo <cmcabedo@gmail.com>
2022-10-27 14:11:55 -0400

    weathercard updated

--- mapboo/src/WeatherCard.js
+++ mapboo/src/WeatherCard.js
@@ -3,18 +3,18 @@ import React from "react";
 export default function WeatherCard() {
   return (
     <div className="weatherCard">
-      <h1>Hi</h1>
-      <h1>aqi</h1>
-      <h1>co</h1>
-      <h1>datetime</h1>
-      <h1>no2</h1>
-      <h1>o3</h1>
-      <h1>pm10</h1>
-      <h1>pm25</h1>
-      <h1>so2</h1>
-      <h1>timestamp_local</h1>
-      <h1>timestamp_utc</h1>
-      <h1>ts</h1>
+      <h1 className="details"><u>Details</u></h1>
+      <h3>AQI: </h3>
+      <h3>Co: </h3>
+      <h3>Date & Time: </h3>
+      <h3>No2: </h3>
+      <h3>O3: </h3>
+      <h3>Pm10: </h3>
+      <h3>Pm25: </h3>
+      <h3>So2: </h3>
+      <h3>Timestamp Local: </h3>
+      <h3>Timestamp Utc: </h3>
+      <h3>TS: </h3>
     </div>
   );
 }
