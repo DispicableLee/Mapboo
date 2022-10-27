5aea1a44bc7a570cc4250c3418c9057aa8aab4be
DispicableLee <lee.robert053@gmail.com>
2022-10-27 13:32:30 -0400

    done

--- mapboo/src/WeatherCard.js
+++ mapboo/src/WeatherCard.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-export default function WeatherCard(props) {
+export default function WeatherCard() {
   return (
     <div>
       <h1>Hi</h1>
-      <h1>{props.aqi}</h1>
-      <h1>{props.co}</h1>
-      <h1>{props.datetime}</h1>
-      <h1>{props.no2}</h1>
-      <h1>{props.o3}</h1>
-      <h1>{props.pm10}</h1>
-      <h1>{props.pm25}</h1>
-      <h1>{props.so2}</h1>
-      <h1>{props.timestamp_local}</h1>
-      <h1>{props.timestamp_utc}</h1>
-      <h1>{props.ts}</h1>
+      <h1>aqi</h1>
+      <h1>co</h1>
+      <h1>datetime</h1>
+      <h1>no2</h1>
+      <h1>o3</h1>
+      <h1>pm10</h1>
+      <h1>pm25</h1>
+      <h1>so2</h1>
+      <h1>timestamp_local</h1>
+      <h1>timestamp_utc</h1>
+      <h1>ts</h1>
     </div>
   );
 }
